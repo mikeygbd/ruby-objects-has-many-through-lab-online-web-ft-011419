dc43afd1f6d9afca80066bb228fc7d5c2b253b17
Michael Soares <mikey.soares@gmail.com>
2019-01-29 15:42:15 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,13 +13,17 @@ attr_accessor :name
     @@all
   end
 
-
     def songs
       Song.all.select  do |song|
         song.genre == self
       end
     end
 
+    def artists
+      Song.all.select  do |song|
+        song.artist == self
+      end
+    end
 
   def new_song(name, genre)
     Song.new(name, artist, self)
